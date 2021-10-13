a323e445d636b9498cadbbf80c737c2b0b397d40
Jonathan Bowman <git@bowmanjd.com>
2021-10-13 11:51:57 -0400

    updates

--- makecontainer.py
+++ makecontainer.py
@@ -3,57 +3,35 @@
 import io
 import json
 import logging
+import os
+import re
 import shlex
 import subprocess
 import time
-from urllib.error import HTTPError
-from urllib.request import urlopen
 
 logging.basicConfig(format="%(message)s", level=logging.INFO)
 
 
-def get_latest(repo, image_name):
+def get_latest(image_name, repo="library", pattern=r"(\d\.?)+", registry="docker.io"):
+    version_re = re.compile(pattern)
+    clean_re = re.compile("[^0-9.]")
     try:
         info = subprocess.check_output(
-            ["skopeo", "inspect", f"docker://docker.io/{repo}/{image_name}"]
+            ["skopeo", "list-tags", f"docker://{registry}/{repo}/{image_name}"]
         )
     except subprocess.CalledProcessError:
         version = ""
     else:
         try:
-            tags = json.loads(info)["RepoTags"]
-            latest = sorted(tuple(map(int, t.split("."))) for t in tags if "." in t)[-1]
-            version = ".".join(str(i) for i in latest)
+            tags = json.loads(info)["Tags"]
+            version_lookup = {
+                tuple(map(int, clean_re.sub("", t).split("."))): t
+                for t in tags
+                if version_re.match(t)
+            }
+            version = version_lookup[max(version_lookup)]
         except (IndexError, ValueError):
             version = ""
-    return version
-
-
-def tag_exists(repo, image_name, tag):
-    url = f"https://hub.docker.com/v2/repositories/{repo}/{image_name}/tags/{tag}/"
-    exists = False
-    try:
-        with urlopen(url) as response:
-            if response.status == 200:
-                exists = True
-    except HTTPError as e:
-        if e.code == 404:
-            exists = False
-        else:
-            raise
-    return exists
-
-
-def latest_alpine_version():
-    version = ""
-    mirror = "https://dl-cdn.alpinelinux.org/alpine"
-    url = f"{mirror}/latest-stable/releases/x86_64/latest-releases.yaml"
-    with urlopen(url) as response:
-        for line in response:
-            row = line.decode()
-            if "version" in row:
-                version = row.split(":")[-1].strip()
-                break
     return version
 
 
@@ -69,6 +47,8 @@ def buildah(cmd):
         for line in proc.stdout:
             output.write(line)
             logging.info(line.strip())
+    if proc.returncode:
+        raise subprocess.CalledProcessError(proc.returncode, proc.args)
     return output.getvalue().strip()
 
 
@@ -77,33 +57,40 @@ def buildah_run(container, cmd):
 
 
 def build_recent(image_name):
-    source_latest = get_latest("library", "alpine")
-    target_latest = get_latest("bowmanjd", image_name)
+    source_latest = get_latest("alpine")
+    target_latest = get_latest(image_name, "bowmanjd")
     if source_latest != target_latest:
         build(image_name, source_latest)
 
 
-def build(image_name, tag):
-    ctr = buildah(f"from docker.io/alpine:{tag}")
+def build(version):
+    ctr = buildah(f"from docker.io/alpine:{version}")
+    tags = [
+        f"{image_name}:{tag}",
+        f"{image_name}:latest",
+        f"{image_name}:{time.strftime('%Y.%m.%d')}",
+    ]
     buildah_run(
         ctr, "apk --no-cache add autoconf automake gcc make musl-dev openssl-dev git"
     )
     buildah(f"config --workingdir /work {ctr}")
     buildah(f"config --label version='{tag}' {ctr}")
     try:
-        image_id = buildah(f"images -q {image_name}:{tag}")
+        image_id = buildah(f"images -q {tags[0]}")
     except subprocess.CalledProcessError:
         pass
     else:
         buildah(f"rmi -f {image_id}")
     buildah(f"commit --squash --rm {ctr} {image_name}:{tag}")
-    buildah(
-        f"tag {image_name}:{tag} {image_name}:latest"
-        f" {image_name}:{time.strftime('%Y.%m.%d')}"
-    )
+    buildah("tag " + " ".join(tags))
+    return tags
+
+
+def push(registry, repo, image_name, tag):
+    pass
 
 
 if __name__ == "__main__":
     # image_name = "alpine-dev"
     # build_recent(image_name)
-    print(get_latest("library", "alpine"))
+    print(get_latest("ubuntu", pattern="focal-(\d\.?)+"))
